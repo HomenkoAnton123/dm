aec77c38079de59ac27d7f4bfeabf92e2ed8fe66
Anton <flame3041@gmail.com>
2026-05-16 15:11:40 +0300

    Delete dm directory

--- dm/bot_instance.py
+++
@@ -1,4 +0,0 @@
-from aiogram import Bot
-
-# Глобальный объект бота, инициализируется в main.py
-bot: Bot = None
--- dm/config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "8878080269:AAFlh-1yI8BjfI9JtSarBXs1bSy3l0yudgg"   # <-- Вставь токен бота сюда
-OWNER_ID = 6141804437               # Твой Telegram ID
-IDLE_TIMEOUT_MINUTES = 5
